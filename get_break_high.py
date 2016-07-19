b27208655376b3d3f0679fc088c6c48364960e3f
rocky v3k <jinweizsu@139.com>
2016-07-19 09:12:12 +0800

    add change table name

--- get_break_high.py
+++ get_break_high.py
@@ -6,15 +6,17 @@ import datetime
 from sqlite_database import SqliteDb
 info=ts.get_stock_basics()
 all_high_stock=[]
-sql_db=SqliteDb()
+sql_db=SqliteDb("_20160719")
 def loop_all_stocks():
-
+    #Óöµ½Í£ÅÆµÄ¡£
     for EachStockID in info.index:
          if is_break_high(EachStockID,60):
              print "High price on",
              print EachStockID,
              print info.ix[EachStockID]['name'].decode('utf-8')
              #sql_db.insert_break_high(all_high_stock)
+
+    sql_db.close()
 
 
 
@@ -60,7 +62,7 @@ def is_break_high(stockID,days):
         #print name.decode('utf-8')
         #print date
         #all_high_stock.append(stock)
-        sql_db.insert_break_high(stock_h)
+        sql_db.store_break_high(stock_h)
         return True
     else:
         return False
--- sqlite_database.py
+++ sqlite_database.py
@@ -9,7 +9,7 @@ __author__ = 'rocchen'
 class SqliteDb():
 
 
-    def __init__(self):
+    def __init__(self,dbtable):
         '''
         self.today = time.strftime("%Y-%m-%d")
         self.DBname = self.today + '.db'
@@ -18,11 +18,12 @@ class SqliteDb():
         today = time.strftime("%Y-%m-%d")
         DBname = today + '.db'
         self.conn = sqlite3.connect(DBname)
-        create_tb = "CREATE TABLE STOCK (date varchar(10),id varchar(6), name varchar(30), p_change REAL,turnover REAL);"
+        self.dbtable=dbtable
+        create_tb = "CREATE TABLE %s (date varchar(10),id varchar(6), name varchar(30), p_change REAL,turnover REAL);" %self.dbtable
         self.conn.execute(create_tb)
         self.conn.commit()
 
-    def insert_break_high(self, price_high_data):
+    def store_break_high(self,price_high_data):
 
         #data 是创新高的个股信息  dataframe
         #print today
@@ -30,7 +31,11 @@ class SqliteDb():
 
         #conn.commit()
         #print "(%s,%s,%f,%f)" %(price_high_data[0], price_high_data[1], price_high_data[2], price_high_data[3])
-        #insert_data_cmd = 'INSERT INTO STOCK(date,id,p_change,turnover) VALUES(?,?,?,?);' % (price_high_data[0], price_high_data[1], price_high_data[2], price_high_data[3])
-
-        self.conn.execute('INSERT INTO STOCK(date,id,name,p_change,turnover) VALUES(?,?,?,?,?)',(price_high_data[0], price_high_data[1], price_high_data[2], price_high_data[3],price_high_data[4]))
+        insert_data_cmd = "INSERT INTO %s(date,id,name,p_change,turnover) VALUES(\"%s\",\"%s\",\"%s\",%f,%f);" %(self.dbtable,price_high_data[0], price_high_data[1], price_high_data[2], price_high_data[3],price_high_data[4])
+        self.conn.execute(insert_data_cmd)
+        #self.conn.execute('INSERT INTO STOCK(date,id,name,p_change,turnover) VALUES(?,?,?,?,?)',(price_high_data[0], price_high_data[1], price_high_data[2], price_high_data[3],price_high_data[4]))
         self.conn.commit()
+
+
+    def close(self):
+        self.conn.close()
