1736af932280db7ce4bd93265e1850d317b98a36
rocky v3k <jinweizsu@139.com>
2016-07-28 22:35:03 +0800

    the same with database

--- get_break_high.py
+++ get_break_high.py
@@ -45,7 +45,6 @@ def is_break_high(stockID,days,fast_type=True):
     today_high=curr_day.iloc[0]['high']
     #这里不能直接用 .values
     #如果用的df【：1】 就需要用.values
-
     #print today_high
     if today_high>=period_high:
         stock_h=[]
